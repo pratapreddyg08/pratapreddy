32ca01afc28a83e2755ed0e353d70dda62c3a2ef
Pratap Reddy <pratapreddyg08@outlook.com>
2020-01-08 21:24:30 +0530

    First commit

---
+++ BLL/rocketBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class rocketBLL
+    {
+    }
+    
+    public class clsBussLayer
+    {
+        rocketDAL objDAL = new rocketDAL();
+
+
+        public void InsertRocket(int Id,string Name,string VehicleType,string Purpose)
+        {
+            objDAL.InsertRocket(Id,Name,VehicleType,Purpose); 
+            
+        }
+        public object SelectRocket()
+        {
+            return objDAL.SelectRocket();
+        }
+        public void UpdateRocket( string Name, string VehicleType, string Purpose, int Id)
+        {
+            objDAL.UpdateRocket( Name, VehicleType,Purpose, Id);
+        }
+        public void DeleteRocket(int Id)
+        {
+             objDAL.DeleteRocket(Id);
+        }
+    }
+}
---
+++ DAL/rocketDAL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace DAL
+{
+   
+    public class rocketDAL
+    {
+        private SqlConnection _SqlCon;
+        public rocketDAL()
+        {
+             _SqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["myconn"].ConnectionString);
+        }
+       
+        public void InsertRocket(int Id, string Name, string VehicleType, string Purpose)
+        {
+            _SqlCon.Open();
+            SqlDataAdapter da = new SqlDataAdapter("Insert into rocketdata values('"+Id+"','"+Name+"','"+VehicleType+"','"+Purpose+"')",_SqlCon);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            _SqlCon.Close();
+        }
+        public object SelectRocket()
+        {
+            _SqlCon.Open();
+            SqlDataAdapter da = new SqlDataAdapter("Select * from rocketdata", _SqlCon);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            _SqlCon.Close();
+            return dt;
+        }
+        public void UpdateRocket(string Name, string VehicleType, string Purpose,int Id)
+        {
+            _SqlCon.Open();
+            SqlDataAdapter da = new SqlDataAdapter("UPDATE rocketdata SET Name='" + Name + "',VehicleType='" + VehicleType + "',Purpose='" + Purpose + "'  WHERE  Id='" + Id + "'", _SqlCon);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            _SqlCon.Close();
+             
+        }
+        public void DeleteRocket(int Id)
+        {
+            _SqlCon.Open();
+            SqlDataAdapter da = new SqlDataAdapter("DELETE FROM rocketdata WHERE Id='"+ Id +"'", _SqlCon);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            _SqlCon.Close();
+        }
+    }
+}
